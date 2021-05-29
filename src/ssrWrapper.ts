67941555d5b1d7c6d5b10f270107dc7f6d95d178
Joshua Flanagan <josh_flanagan@live.co.uk>
2021-05-29 17:54:16 +0100

    Added meta fb and twitter

--- src/ssrWrapper.ts
+++ src/ssrWrapper.ts
@@ -23,9 +23,26 @@ const Wrapper = (props: HtmlProps) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta type="description" content="A blog for a web developer to ramble into">
-    ${props.meta}
+    <meta name="description" content="A blog for a web developer to ramble into">
+    <!-- Primary Meta Tags -->
     <title>${props.title || 'title'}</title>
+    <meta name="title" content="🖥️JFlagg's Blog">
+    <meta name="description" content="A blog for a web developer to ramble into">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.uploadr.co.uk/blog">
+    <meta property="og:title" content="🖥️JFlagg's Blog">
+    <meta property="og:description" content="A blog for a web developer to ramble into">
+    <meta property="og:image" content="https://avatars.githubusercontent.com/u/23509159?v=4">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://www.uploadr.co.uk/blog">
+    <meta property="twitter:title" content="🖥️JFlagg's Blog">
+    <meta property="twitter:description" content="A blog for a web developer to ramble into">
+    <meta property="twitter:image" content="https://avatars.githubusercontent.com/u/23509159?v=4">
+    ${props.meta}
     <style>${props.styles}</style>
 </head>
 <body>
